094887adbff2c4924fc9a7491b993ce470bf9cb3
leesion29 <leesion29@naver.com>
2025-03-10 17:38:46 +0900

    백엔드 프로젝트 파일 추가

---
+++ backend/academyManager/src/main/java/com/green/codeweavers/academymanager/dto/UserDTO.java
@@ -0,0 +1,13 @@
+package com.green.codeweavers.academymanager.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+// 계층 간 데이터 교환을 하기 위해 사용하는 멤버 객체
+// user를 extends해도 됩니다
+@Getter
+@Setter
+@ToString
+public class UserDTO {
+}
