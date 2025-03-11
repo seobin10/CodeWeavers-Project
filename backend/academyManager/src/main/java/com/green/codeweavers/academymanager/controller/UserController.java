07e146e40301126517c7b31bd287769cace5daa5
leesion29 <leesion29@naver.com>
2025-03-11 11:14:24 +0900

    cwdb에 연동되도록 설정 수정

--- backend/academyManager/src/main/java/com/green/codeweavers/academymanager/controller/UserController.java
+++ backend/academyManager/src/main/java/com/green/codeweavers/academymanager/controller/UserController.java
@@ -2,10 +2,12 @@ package com.green.codeweavers.academymanager.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 // 교수, 학생 마이페이지의 컨트롤러
 @Controller
+@RequestMapping("/user")
 public class UserController {
     @ResponseBody
     @GetMapping("/professor")
