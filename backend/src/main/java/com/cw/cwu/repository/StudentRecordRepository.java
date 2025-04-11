34602e2a2d4b75850d45a52997196fe1f9a870ff
seohyeokbin <seohyeokbin@gmail.com>
2025-04-11 15:14:54 +0900

    비관적 락 도딥 중

--- backend/src/main/java/com/cw/cwu/repository/StudentRecordRepository.java
+++ backend/src/main/java/com/cw/cwu/repository/StudentRecordRepository.java
@@ -7,9 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface StudentRecordRepository extends JpaRepository<StudentRecord, Long> {
 
-
-    boolean existsByStudentAndSemester(User student, Semester semester);
+    Optional<StudentRecord> findByStudentAndSemester(User student, Semester semester);
 }
 
