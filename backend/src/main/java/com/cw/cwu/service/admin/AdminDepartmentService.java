127fd93af397b6610e3b5765d58c1b48eaf71d44
seobin10 <seohyeokbin@naver.com>
2025-04-24 08:14:37 +0900

    학과 CRUD 기능 구현중

--- backend/src/main/java/com/cw/cwu/service/admin/AdminDepartmentService.java
+++ backend/src/main/java/com/cw/cwu/service/admin/AdminDepartmentService.java
@@ -1,7 +1,16 @@
 package com.cw.cwu.service.admin;
 
+import com.cw.cwu.dto.DepartmentCreateRequestDTO;
 import com.cw.cwu.dto.DepartmentDetailDTO;
+import com.cw.cwu.dto.DepartmentUpdateRequestDTO;
 
 public interface AdminDepartmentService {
     DepartmentDetailDTO getDepartmentDetail(Integer id);
+
+
+    void createDepartment(DepartmentCreateRequestDTO dto);
+
+    void updateDepartment(Integer id, DepartmentUpdateRequestDTO dto);
+
+    void deleteDepartment(Integer id);
 }
--- backend/src/main/java/com/cw/cwu/service/admin/AdminDepartmentServiceImpl.java
+++ backend/src/main/java/com/cw/cwu/service/admin/AdminDepartmentServiceImpl.java
@@ -1,8 +1,11 @@
 package com.cw.cwu.service.admin;
 
 import com.cw.cwu.domain.Department;
+import com.cw.cwu.domain.DepartmentStatus;
 import com.cw.cwu.domain.UserRole;
+import com.cw.cwu.dto.DepartmentCreateRequestDTO;
 import com.cw.cwu.dto.DepartmentDetailDTO;
+import com.cw.cwu.dto.DepartmentUpdateRequestDTO;
 import com.cw.cwu.repository.ClassEntityRepository;
 import com.cw.cwu.repository.CourseRepository;
 import com.cw.cwu.repository.DepartmentRepository;
@@ -39,4 +42,52 @@ public class AdminDepartmentServiceImpl implements AdminDepartmentService {
                 classCount
         );
     }
+
+
+    @Override
+    public void createDepartment(DepartmentCreateRequestDTO dto) {
+        if (departmentRepository.existsByDepartmentName(dto.getDepartmentName())) {
+            throw new IllegalArgumentException("이미 존재하는 학과입니다.");
+        }
+
+        Department dept = Department.builder()
+                .departmentName(dto.getDepartmentName())
+                .status(DepartmentStatus.AVAILABLE)
+                .build();
+
+        departmentRepository.save(dept);
+    }
+
+
+    @Override
+    public void updateDepartment(Integer id, DepartmentUpdateRequestDTO dto) {
+        Department dept = departmentRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("학과가 존재하지 않음"));
+
+        if (dto.getNewName() != null && !dto.getNewName().isBlank()) {
+            // 중복 이름 방지
+            if (departmentRepository.existsByDepartmentName(dto.getNewName()) &&
+                    !dto.getNewName().equals(dept.getDepartmentName())) {
+                throw new IllegalArgumentException("이미 존재하는 학과명입니다.");
+            }
+            dept.setDepartmentName(dto.getNewName());
+        }
+
+        if (dto.getNewStatus() != null) {
+            dept.setStatus(dto.getNewStatus());
+        }
+
+        departmentRepository.save(dept);
+    }
+
+    @Override
+    public void deleteDepartment(Integer id) {
+        if (!departmentRepository.existsById(id)) {
+            throw new EntityNotFoundException("학과가 존재하지 않음");
+        }
+
+        departmentRepository.deleteById(id);
+    }
+
+
 }
